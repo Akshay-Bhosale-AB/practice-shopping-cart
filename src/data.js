4f5ca3383959eeb84169833db82a96d63bae9733
Akshay <akshaybhosale77ab@gmail.com>
2021-08-24 21:50:35 +0530

    this is my another commit

---
+++ src/data.js
@@ -0,0 +1,36 @@
+
+const data = {
+    products:[{
+        id:'1',
+        name:'Macbook',
+        price:4500,
+        image:'./images/lap-1.jpg'
+    },
+    {
+        id:'2',
+        name:'iPhone',
+        price:1500,
+        image:'./images/mob-1.jpg'
+    },
+    {
+        id:'3',
+        name:'iPad',
+        price:2500,
+        image:'./images/mob-1.jpg'
+    },
+    {
+        id:'4',
+        name:'iPod',
+        price:1000,
+        image:'./images/mob-1.jpg'
+    },
+    {
+        id:'5',
+        name:'Headphones',
+        price:499,
+        image:'./images/mob-1.jpg'
+    },
+],
+};
+
+export default data;
